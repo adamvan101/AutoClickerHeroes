ecb5889408b898280e466055f029bbd595903206
adamvan101 <avanaken92@gmail.com>
2014-12-04 23:43:21 -0500

    Initial Commit

---
+++ AutoClicker/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace AutoClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.setPos = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.checkbox_level = new System.Windows.Forms.CheckBox();
+            this.checkbox_keyboard = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.start_Click);
+            // 
+            // setPos
+            // 
+            this.setPos.Location = new System.Drawing.Point(12, 85);
+            this.setPos.Name = "setPos";
+            this.setPos.Size = new System.Drawing.Size(90, 23);
+            this.setPos.TabIndex = 2;
+            this.setPos.Text = "Set Position";
+            this.setPos.UseVisualStyleBackColor = true;
+            this.setPos.Click += new System.EventHandler(this.setPos_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 59);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(90, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Click Interval:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "milliseconds",
+            "seconds",
+            "minutes"});
+            this.comboBox1.Location = new System.Drawing.Point(110, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(90, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 24);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Please Set Position";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Number of Alternate Points:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(149, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Add Point";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(110, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Clear Points";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 167);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Execute Points";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(108, 167);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Set Click";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // checkbox_level
+            // 
+            this.checkbox_level.AutoSize = true;
+            this.checkbox_level.Location = new System.Drawing.Point(15, 196);
+            this.checkbox_level.Name = "checkbox_level";
+            this.checkbox_level.Size = new System.Drawing.Size(78, 17);
+            this.checkbox_level.TabIndex = 8;
+            this.checkbox_level.Text = "Level Click";
+            this.checkbox_level.UseVisualStyleBackColor = true;
+            // 
+            // checkbox_keyboard
+            // 
+            this.checkbox_keyboard.AutoSize = true;
+            this.checkbox_keyboard.Location = new System.Drawing.Point(108, 196);
+            this.checkbox_keyboard.Name = "checkbox_keyboard";
+            this.checkbox_keyboard.Size = new System.Drawing.Size(71, 17);
+            this.checkbox_keyboard.TabIndex = 9;
+            this.checkbox_keyboard.Text = "Keyboard";
+            this.checkbox_keyboard.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(211, 218);
+            this.Controls.Add(this.checkbox_keyboard);
+            this.Controls.Add(this.checkbox_level);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.setPos);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "AutoClicker";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button setPos;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.CheckBox checkbox_level;
+        private System.Windows.Forms.CheckBox checkbox_keyboard;
+    }
+}
+
---
+++ AutoClicker/Form1.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoClicker
+{   
+    public partial class Form1 : Form
+    {
+        // Define our mouse event constants
+        const int MOUSEEVENTF_LEFTDOWN = 2;
+        const int MOUSEEVENTF_LEFTUP = 4;
+
+        // This is a constant for our input type
+        const int INPUT_MOUSE = 0;
+
+        // A struct that holds the Mouse Input information
+        public struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        // My screen coords for Treebeast
+        Point _hero = new Point(267, 592);
+
+        // A struct of input information to pass to the "click" call
+        public struct INPUT
+        {
+            public uint type;
+            public MOUSEINPUT mi;
+        };
+
+        // A reference to the hotkey for our code
+        const int F6_HOTKEY_ID = 1;
+        const int F7_HOTKEY_ID = 2;
+        const int F8_HOTKEY_ID = 3;
+        const int F8_HOTKEY_ID_SHIFT = 4;
+        const int F9_HOTKEY_ID = 5;
+        const int F9_HOTKEY_ID_SHIFT = 6;
+        const int F6_HOTKEY_ID_SHIFT = 7;
+        
+        // Some private variables to store data
+        Point mousePosition = new Point(0, 0);
+        List<Point> alternatePoints = new List<Point>();
+        Timer setPoint = new Timer();
+        Timer clickTimer = new Timer();
+        Timer keyTimer = new Timer();
+        Timer levelTimer = new Timer();
+        int countdown;
+        int index = 0;
+        bool _lockMouse;
+        bool executingAlts = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Timers
+            setPoint.Tick += setPoint_Tick;
+            clickTimer.Tick += clickTimer_Tick;
+            keyTimer.Tick += keyTimer_Tick;
+            levelTimer.Tick += levelTimer_Tick;
+
+            // misc.
+            comboBox1.SelectedIndex = 0;
+            numericUpDown1.Value = 1;
+            button1.Enabled = false;
+
+            // Hotkeys
+            RegisterHotKey(this.Handle, F6_HOTKEY_ID, 0, (int)Keys.F6);
+            RegisterHotKey(this.Handle, F6_HOTKEY_ID_SHIFT, 4, (int)Keys.F6);
+            RegisterHotKey(this.Handle, F7_HOTKEY_ID, 0, (int)Keys.F7);
+            RegisterHotKey(this.Handle, F8_HOTKEY_ID, 0, (int)Keys.F8);
+            RegisterHotKey(this.Handle, F8_HOTKEY_ID_SHIFT, 4, (int)Keys.F8);
+            RegisterHotKey(this.Handle, F9_HOTKEY_ID, 0, (int)Keys.F9);
+            RegisterHotKey(this.Handle, F9_HOTKEY_ID_SHIFT, 4, (int)Keys.F9);
+
+            try
+            {
+                // Read file
+                System.Xml.Serialization.XmlSerializer reader =
+                    new System.Xml.Serialization.XmlSerializer(typeof(List<Point>));
+                var desktopFodler = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                var fullName = Path.Combine(desktopFodler, "List.txt");
+
+                System.IO.StreamReader file = new System.IO.StreamReader(
+                 fullName);
+                alternatePoints = (List<Point>)reader.Deserialize(file);
+                label4.Text = alternatePoints.Count.ToString();
+            }
+            catch (Exception e)
+            {
+                // Suppress
+            }
+        }
+
+        void levelTimer_Tick(object sender, EventArgs e)
+        {
+            if (clickTimer.Enabled && checkbox_level.Checked)
+            {
+                levels();
+            }
+        }
+
+        void levels()
+        {
+            Point p = Cursor.Position;
+            executingAlts = true;
+            System.Threading.Thread.Sleep(100);
+            ClickPoint(_hero);
+            System.Threading.Thread.Sleep(100);
+            executingAlts = false;
+            Cursor.Position = p;
+        }
+
+        void Keyboard()
+        {
+            System.Windows.Forms.SendKeys.Send("A");
+            System.Threading.Thread.Sleep(40);
+
+            System.Windows.Forms.SendKeys.Send("8");
+            System.Threading.Thread.Sleep(40);
+            System.Windows.Forms.SendKeys.Send("7");
+            System.Threading.Thread.Sleep(40);
+            System.Windows.Forms.SendKeys.Send("9");
+            System.Threading.Thread.Sleep(40);
+            System.Windows.Forms.SendKeys.Send("1");
+            System.Threading.Thread.Sleep(40);
+            System.Windows.Forms.SendKeys.Send("2");
+            System.Threading.Thread.Sleep(40);
+            System.Windows.Forms.SendKeys.Send("3");
+            System.Threading.Thread.Sleep(40);
+            System.Windows.Forms.SendKeys.Send("4");
+            System.Threading.Thread.Sleep(40);
+            System.Windows.Forms.SendKeys.Send("5");
+            System.Threading.Thread.Sleep(40);
+            System.Windows.Forms.SendKeys.Send("6");
+            System.Threading.Thread.Sleep(40);
+        }
+
+
+        void keyTimer_Tick(object sender, EventArgs e)
+        {
+            if (clickTimer.Enabled && checkbox_keyboard.Checked)
+            {
+                Keyboard();
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == F6_HOTKEY_ID_SHIFT)
+            {
+                mousePosition = Cursor.Position;
+                StartClicking(false);
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == F6_HOTKEY_ID)
+            {
+                mousePosition = Cursor.Position;
+                StartClicking(true);
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == F7_HOTKEY_ID)
+            {
+                StopClicking();
+                this.WindowState = FormWindowState.Minimized;
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == F8_HOTKEY_ID)
+            {
+                ExecuteAlternatePoints();
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == F8_HOTKEY_ID_SHIFT)
+            {
+                ExecuteNextPoint();
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == F9_HOTKEY_ID)
+            {
+                IncrementIndex(1);
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == F9_HOTKEY_ID_SHIFT)
+            {
+                IncrementIndex(-1);
+            }
+            base.WndProc(ref m);
+        }
+
+        void clickTimer_Tick(object sender, EventArgs e)
+        {
+            if (executingAlts)
+            {
+                return;
+            }
+
+            //if (_lockMouse)
+            //{
+                Point p = Cursor.Position;
+                ClickPoint(mousePosition);
+                Cursor.Position = p;
+            //}
+            //else
+            //{
+                //ClickPoint(Cursor.Position);
+            //}
+        }
+
+        void ClickPoint(Point p)
+        {
+            // Move our cursor
+            Cursor.Position = p;
+            // Set our structs
+            INPUT i = new INPUT();
+            i.type = INPUT_MOUSE;
+            i.mi.dx = 0;
+            i.mi.dy = 0;
+            i.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+            i.mi.dwExtraInfo = IntPtr.Zero;
+            i.mi.mouseData = 0;
+            i.mi.time = 0;
+            // Send our mouse down input
+            SendInput(1, ref i, Marshal.SizeOf(i));
+            // Now send mouse up input
+            i.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+            SendInput(1, ref i, Marshal.SizeOf(i));
+        }
+
+        void setPoint_Tick(object sender, EventArgs e)
+        {
+            if (countdown == 0)
+            {
+                mousePosition = Cursor.Position;
+                //show the location on window title
+                this.label2.Text = "Position Set";
+                setPoint.Stop();
+                button1.Enabled = true;
+            }
+            else
+            {
+                setPoint.Interval = 1000;
+                this.label2.Text = "Setting Position In " + countdown.ToString();
+                countdown--;
+            }
+        }
+
+        // Dlls used to handle input. Are they different?
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern int SendInput(int nInputs, ref INPUT pInputs, int cbSize);
+        //[DllImport("user32.dll")]
+        //public static extern IntPtr SetCapture(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            e.SuppressKeyPress = true; // do this to 'eat' the keystroke
+            if (e.KeyCode == Keys.Escape)
+            {
+                StopClicking();
+            }
+            base.OnKeyDown(e);
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value <= 0)
+            {
+                numericUpDown1.Value = 1;
+            }
+            switch (comboBox1.SelectedIndex)
+            {
+                default:
+                case 0:
+                    clickTimer.Interval = (int)((float)numericUpDown1.Value);
+                    break;
+                case 1:
+                    clickTimer.Interval = (int)((float)numericUpDown1.Value * 1000);
+                    break;
+                case 2: 
+                    clickTimer.Interval = (int)((float)numericUpDown1.Value * 1000 * 60);
+                    break;
+            }
+            if (!clickTimer.Enabled && button1.Enabled)
+            {
+                StartClicking(true);
+            }
+            else
+            {
+                StopClicking();
+            }
+        }
+
+        private void StopClicking()
+        {
+            clickTimer.Stop();
+            this.label2.Text = "Stopped";
+            button1.Text = "Start";
+            numericUpDown1.Enabled = true;
+            setPos.Enabled = true;
+            comboBox1.Enabled = true;
+        }
+
+        private void StartClicking(bool lockMouse)
+        {
+            _lockMouse = lockMouse;
+            clickTimer.Start();
+            this.label2.Text = "Clicking";
+            button1.Text = "Stop";
+            numericUpDown1.Enabled = false;
+            setPos.Enabled = false;
+            comboBox1.Enabled = false;
+
+            if (lockMouse)
+            {
+                // The keyboard timer
+                keyTimer.Interval = 1000 * 60 * 30;
+                keyTimer.Start();
+                Keyboard();
+
+                // The level timer
+                levelTimer.Interval = 1000 * 60;
+                levelTimer.Start();
+                levels();
+            }
+        }
+
+        private void setPos_Click(object sender, EventArgs e)
+        {
+            countdown = 3;
+            setPoint.Interval = 1;
+            setPoint.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Add point
+            alternatePoints.Add(Cursor.Position);
+            label4.Text = alternatePoints.Count.ToString();
+
+            try
+            {
+                System.Xml.Serialization.XmlSerializer writer =
+                    new System.Xml.Serialization.XmlSerializer(typeof(List<Point>));
+                var desktopFodler = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                var fullName = Path.Combine(desktopFodler, "List.txt");
+
+                System.IO.StreamWriter file = new System.IO.StreamWriter(
+                 fullName);
+                writer.Serialize(file, alternatePoints);
+                file.Close();
+            }
+            catch
+            {
+                // suppress for now
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Clear points
+            alternatePoints.Clear();
+            label4.Text = alternatePoints.Count.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Execute points
+            ExecuteAlternatePoints();
+        }
+
+        private void ExecuteAlternatePoints()
+        {
+            executingAlts = true;
+
+            foreach (Point p in alternatePoints)
+            {
+                ClickPoint(p);
+                System.Threading.Thread.Sleep(600);
+            }
+
+            executingAlts = false;
+        }
+
+        private void ExecuteNextPoint()
+        {
+            if (alternatePoints.Count == 0)
+            {
+                return;
+            }
+
+            ClickPoint(alternatePoints[index]);
+        }
+
+        private void IncrementIndex(int i)
+        {
+            index += i;
+            if (index >= alternatePoints.Count)
+            {
+                index = 0;
+            }
+            else if (index < 0)
+            {
+                index = alternatePoints.Count - 1;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //ExecuteNextPoint();
+            _hero = Cursor.Position;
+        }
+    }
+}
